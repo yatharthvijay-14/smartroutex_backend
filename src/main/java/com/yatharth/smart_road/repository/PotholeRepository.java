e25267a9c5add70f8fdd9dc200db434a65d76f50
Yatharth vijay <yashuvj7@gmail.com>
2026-08-29 18:58:19 +0530

    Add user-filtered pothole endpoints and seed data user tagging

--- src/main/java/com/yatharth/smart_road/repository/PotholeRepository.java
+++ src/main/java/com/yatharth/smart_road/repository/PotholeRepository.java
@@ -2,11 +2,9 @@ package com.yatharth.smart_road.repository;
 
 import com.yatharth.smart_road.entity.Pothole;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PotholeRepository extends JpaRepository<Pothole, Long> {
 
     List<Pothole> findBySeverity(String severity);
